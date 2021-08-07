6d8d12d8e4b839d7ab2f00f5a7d6ceae6c32e3ce
sharletmvarghese03 <83110256+sharletmvarghese03@users.noreply.github.com>
2021-08-07 22:11:16 +0530

    Initial Commit
    uploading all files to server.

---
+++ Boiled_Egg_Timer/Boiled_Egg_Timer/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Boiled_Egg_Timer
+//
+//  Created by Sharlet Varghese on 07/08/21.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+ 
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)          //print(progressBar.progress)
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+            playSound()
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+}
